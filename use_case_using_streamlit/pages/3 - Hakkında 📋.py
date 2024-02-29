898ad608121aff275c092e3ee2813978feddc549
fatih <fatihsalihp@gmail.com>
2024-02-29 03:01:29 +0300

    adding use case streamlit and additional files.

---
+++ use_case_using_streamlit/pages/3 - Hakkında 📋.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.set_page_config(layout="centered")
+st.markdown("""
+<span style="font-weight:bold; font-family:Trebuchet MS, sans-serif;">Bu finansal dashboard uygulaması yalnızca bilgi amaçlıdır. Bu uygulama, kullanıcıya finansal verileri görselleştirmek ve sunmak için tasarlanmıştır. Ancak, bu uygulama tarafından sunulan herhangi bir bilginin doğruluğu, eksiksizliği veya güncelliği konusunda garanti verilmemektedir.</span>\n\n  
+<span style="font-weight:bold; font-family:Trebuchet MS, sans-serif;">Bu uygulama, finansal analizler, tahminler veya yatırım kararları için bir tavsiye veya öneri sağlamaz. Kullanıcılar, bu uygulama tarafından sağlanan verilere dayanarak finansal kararlar almadan önce kendi araştırmalarını yapmalı ve gerektiğinde profesyonel bir finansal danışmana başvurmalıdır.</span>\n\n  
+<span style="font-weight:bold; font-family:Trebuchet MS, sans-serif;">Bu uygulama tarafından sunulan herhangi bir bilginin neden olduğu doğrudan veya dolaylı zararlardan veya kayıplardan sorumlu tutulamaz. Kullanıcılar, bu uygulamayı kullanırken kendi risklerini üstlenirler ve bu uygulamanın kullanımından kaynaklanan herhangi bir sonuçtan kendileri sorumludur.</span>\n\n  
+""", unsafe_allow_html=True)
